86797d091c7206610d83c5b2cae8d852fef01386
mumpf <waldemar@porscha.eu>
2015-09-20 17:29:07 +0200

    Support for ETS5 .knxprod project files

--- json2conf/ExtractGA.cs
+++ json2conf/ExtractGA.cs
@@ -8,55 +8,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace json2conf
-{
-    class ExtractGA
-    {
+namespace json2conf {
+    class ExtractGA {
         public SortedDictionary<string, List<string>> GA = new SortedDictionary<string, List<string>>(new Util());
 
-        private void ExtractEtsGA(string iFileName)
-        {
+        private void ExtractEtsGA(string iFileName) {
             //string lFileName = Path.Combine(iDirName, "0.xml");
-            XDocument xElement = XDocument.Load(iFileName);
-            foreach (var lElement in xElement.Descendants("{http://knx.org/xml/project/11}GroupAddress"))
-            {
+            XElement xElement = XElement.Load(iFileName);
+            var lXmlns = xElement.Attribute("xmlns").Value;
+            foreach (var lElement in xElement.Descendants("{" + lXmlns + "}GroupAddress")) {
                 var lGa = OutputGA(lElement.Attribute("Address").Value);
                 var lName = lElement.Attribute("Name").Value;
-                Debug.WriteLine(string.Format("{0} - {1}", lGa , lName ));
+                Debug.WriteLine(string.Format("{0} - {1}", lGa, lName));
                 var lList = new List<string>();
                 lList.Add(lName);
                 GA.Add(lGa, lList);
             }
         }
 
-        private string OutputGA(string iGaDezimal)
-        {
+        private string OutputGA(string iGaDezimal) {
             int lGaDezimal = Convert.ToInt16(iGaDezimal);
             return string.Format("{0}/{1}/{2}", (lGaDezimal & 0x7800) >> 11, (lGaDezimal & 0x700) >> 8, lGaDezimal & 0xff);
         }
 
-        private void ExtractFileFromZip(string iZipFileName, string iEntryName, string iExtractionTarget)
-        {
+        private void ExtractFileFromZip(string iZipFileName, string iEntryName, string iExtractionTarget) {
             using (ZipArchive zip = ZipFile.Open(iZipFileName, ZipArchiveMode.Read))
                 foreach (ZipArchiveEntry entry in zip.Entries)
-                    if (entry.Name == iEntryName)
-                    {
+                    if (entry.Name == iEntryName) {
                         entry.ExtractToFile(iExtractionTarget, true);
                         File.SetLastAccessTimeUtc(iExtractionTarget, DateTime.UtcNow);
                     }
         }
 
-        public bool ExtractEtsProject(string iEtsProjectFileName)
-        {
+        public bool ExtractEtsProject(string iEtsProjectFileName) {
             //check if ets project file is given
-            if (Path.GetExtension(iEtsProjectFileName) == ".knxproj")
-            {
+            if (Path.GetExtension(iEtsProjectFileName) == ".knxproj") {
                 //get temp file name
                 string lTempPath = Path.Combine(Path.GetTempPath(), "json2conf");
-                string lTempFileName = Path.Combine(lTempPath, "0.xml");
-                if (!File.Exists(lTempFileName) || File.GetLastAccessTimeUtc(iEtsProjectFileName) > File.GetLastAccessTimeUtc(lTempFileName))
-                {
-                    if (!Directory.Exists(lTempPath)) Directory.CreateDirectory(lTempPath); 
+                string lTempFileName = Path.Combine(lTempPath, Path.GetFileNameWithoutExtension(iEtsProjectFileName) + "-0.xml");
+                if (!File.Exists(lTempFileName) || File.GetLastAccessTimeUtc(iEtsProjectFileName) > File.GetLastAccessTimeUtc(lTempFileName)) {
+                    if (!Directory.Exists(lTempPath)) Directory.CreateDirectory(lTempPath);
                     //We have a knxproj file, which is newer than out cached GA file
                     //We extract the newer GA file from the knxproj file
                     ExtractFileFromZip(iEtsProjectFileName, "0.xml", lTempFileName);
